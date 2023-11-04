70f6831a84a33aba688ff3eaa68f184bf2270eff
Sayandeep Paul <104418784+sayandeeppaul@users.noreply.github.com>
2023-11-04 20:01:16 +0000

    create react app through vite

--- 02_vite_react_app/src/main.jsx
+++ 02_vite_react_app/src/main.jsx
@@ -3,8 +3,48 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// const reactElement = {
+//   type:'a',
+//   props:{
+//       href :'https://google.com',
+//       target:'_blank',
+//       class:'link',
+//       id:'google',
+//       title:'demo'
+//   },
+//   children : "click me to open google"
+// }
+
+// let anotherElement = (
+//   <a  href='https://google.com' target='_blank'>google</a>
+// )
+
+
+
+function MyApp() {
+
+  let a = 2
+  let b = 4
+  let c = a + b
+
+  return (
+    <div>
+      <h1>Sayandeep {a} + {b} = {c}</h1> 
+    </div>
+  )
+}
+const textInsideTag = "click me to open google"
+
+const reactElement = React.createElement(
+  'a',
+  {
+    href :'https://google.com',
+      target:'_blank',
+  },
+  textInsideTag
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <MyApp />
+  // reactElement
 )
